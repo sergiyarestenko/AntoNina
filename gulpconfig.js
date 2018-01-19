27f3c72b506129f5c225ee536914d5d1d1fa4b3a
ArestenkoS <arestenkos@gmail.com>
2018-01-19 07:10:22 +0300

    finished city pages

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,25 +1,25 @@
 "use strict";
 const os = require('os');
-const browser = os.platform() === 'linux' ? 'google-chrome' : (os.platform() === 'darwin' ? 'google chrome' : (os.platform() === 'win32' ? 'chrome': 'firefox' ));
+const browser = os.platform() === 'linux' ? 'google-chrome' : (os.platform() === 'darwin' ? 'google chrome' : (os.platform() === 'win32' ? 'chrome' : 'firefox'));
 
 module.exports = {
-    port: 8000,
+  port: 8000,
+  browser: browser,
+  paths: {
+    src: "./src",
+    build: "./build",
+    html: "./src/**/*.html",
+    mainHtml: "./src/**.html",
+    templateLess: "./src/templates/**/*.less",
+    less: "./src/stylesheets/**.less",
+    mainLess: "./src/stylesheets/main.less",
+    js: './src/js/**.js',
+    img: './src/img/**/*.*'
+  },
+  browserSync: {
+    proxy: 'http://localhost:8000/index.html',
+    files: ['build/**/*.*'],
     browser: browser,
-    paths: {
-        src: "./src",
-        build: "./build",
-        html: "./src/**/*.html",
-        mainHtml: "./src/**.html",
-        templateLess:"./src/templates/**/*.less",
-        less:"./src/stylesheets/**.less",
-        mainLess:"./src/stylesheets/main.less",
-        js: './src/js/**.js',
-        img: './src/img/**/*.*'
-    },
-    browserSync: {
-        proxy: 'http://localhost:8000/index.html',
-        files: ['build/**/*.*'],
-        browser: browser,
-        port: 8001
-    }
+    port: 8001
+  }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
 'use strict';
 
-const   config = require('./gulpconfig.js'),
-	gulp = require('gulp'),
-	rigger = require('gulp-rigger'),
-	less = require('gulp-less'),
-	lesshint = require('gulp-lesshint'),
-	autoprefixer = require('gulp-autoprefixer'),
-	cleancss = require('gulp-clean-css'),
-	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	connect = require('gulp-connect'),
-	gutil = require('gulp-util'),
-	browserSync = require('browser-sync').create();
+const config = require('./gulpconfig.js'),
+  gulp = require('gulp'),
+  rigger = require('gulp-rigger'),
+  less = require('gulp-less'),
+  lesshint = require('gulp-lesshint'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleancss = require('gulp-clean-css'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  connect = require('gulp-connect'),
+  gutil = require('gulp-util'),
+  browserSync = require('browser-sync').create();
 
 
-gulp.task('css',function() {
-return gulp.src(config.paths.mainLess)
-	.pipe(less())
-	.on('error', function(err) {
-	const type = err.type || '';
-	const message = err.message || '';
-	const extract = err.extract || [];
-	const line = err.line || '';
-	const column = err.column || '';
-	gutil.log(gutil.colors.red.bold('[Less error]') +' '+ gutil.colors.bgRed(type) +' ('+ line +':'+ column +')');
-	gutil.log(gutil.colors.bold('message:') +' '+ message);
-	gutil.log(gutil.colors.bold('codeframe:') +'\n'+ extract.join('\n'));
-	this.emit('end');
-	})
-	.pipe(autoprefixer())
-	// .pipe(cleancss({compatibility: 'ie8'}))
-	.pipe(gulp.dest(config.paths.build +'/stylesheets'));
+gulp.task('css', function() {
+  return gulp.src(config.paths.mainLess)
+    .pipe(less())
+    .on('error', function(err) {
+      const type = err.type || '';
+      const message = err.message || '';
+      const extract = err.extract || [];
+      const line = err.line || '';
+      const column = err.column || '';
+      gutil.log(gutil.colors.red.bold('[Less error]') + ' ' + gutil.colors.bgRed(type) + ' (' + line + ':' + column + ')');
+      gutil.log(gutil.colors.bold('message:') + ' ' + message);
+      gutil.log(gutil.colors.bold('codeframe:') + '\n' + extract.join('\n'));
+      this.emit('end');
+    })
+    .pipe(autoprefixer())
+    // .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest(config.paths.build + '/stylesheets'));
 });
 
 
@@ -38,57 +38,59 @@ return gulp.src(config.paths.mainLess)
 
 
 
-gulp.task('lessLint',function(){
-    return gulp.src(config.paths.less)
-	    .pipe(lesshint())
-	    .pipe(lesshint.reporter('reporter-name'));
-	    // .pipe(lesshint.failOnError()); // Use this to fail the task on lint errors
+gulp.task('lessLint', function() {
+  return gulp.src(config.paths.less)
+    .pipe(lesshint())
+    .pipe(lesshint.reporter('reporter-name'));
+  // .pipe(lesshint.failOnError()); // Use this to fail the task on lint errors
 });
 
 
 
-gulp.task('js',function(){
-    return gulp.src(config.paths.js)
-        // .pipe(uglify())
-        .pipe(gulp.dest(config.paths.build + '/js'))
+gulp.task('js', function() {
+  return gulp.src(config.paths.js)
+    // .pipe(uglify())
+    .pipe(gulp.dest(config.paths.build + '/js'))
 });
 
 
 //html task
-gulp.task('html',function(){
-    return gulp.src(config.paths.mainHtml)
-        .pipe(rigger())
-        .pipe(gulp.dest(config.paths.build))
+gulp.task('html', function() {
+  return gulp.src(config.paths.mainHtml)
+    .pipe(rigger())
+    .pipe(gulp.dest(config.paths.build))
 });
 
 //img task
-gulp.task('img',function(){
-    return gulp.src(config.paths.img)
-        .pipe(imagemin())
-        .pipe(gulp.dest(config.paths.build + '/img'))
+gulp.task('img', function() {
+  return gulp.src(config.paths.img)
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.paths.build + '/img'))
 });
 
 //server
-gulp.task('server', function(){
-    connect.server({root: config.paths.build, port: config.port});
+gulp.task('server', function() {
+  connect.server({
+    root: config.paths.build,
+    port: config.port
+  });
 });
 
 //Gulp serve task - initializes browser synchronization
 gulp.task('serve', ['server'], function() {
-    browserSync.init(null, config.browserSync);
+  browserSync.init(null, config.browserSync);
 });
 
 
 //watchers
-gulp.task('watch', function(){
-    // gulp.watch(config.paths.img, ['img']);
-    gulp.watch(config.paths.templateLess, [/*'lessLint',*/'css']);
-    gulp.watch(config.paths.less, [/*'lessLint',*/'css']);
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.mainHtml, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+gulp.task('watch', function() {
+  // gulp.watch(config.paths.img, ['img']);
+  gulp.watch(config.paths.templateLess, [ /*'lessLint',*/ 'css']);
+  gulp.watch(config.paths.less, [ /*'lessLint',*/ 'css']);
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.mainHtml, ['html']);
+  gulp.watch(config.paths.js, ['js']);
 });
 
 
-gulp.task('default', ['img',/*'lessLint',*/'css', 'js', 'html', 'serve', 'watch']);
-
+gulp.task('default', ['img', /*'lessLint',*/ 'css', 'js', 'html', 'serve', 'watch']);
