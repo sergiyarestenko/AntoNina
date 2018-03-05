86af2024faf8abbd6c2d8e5cf3fc3eb09a591088
sergiyarestenko <arestenkos@gmail.com>
2018-03-05 08:58:58 +0200

    registration ready

--- src/js/_enter_form.js
+++ src/js/_enter_form.js
@@ -1,8 +1,8 @@
 this.openEnterForm = function () {
     enterWrapper.addClass('gt-open');
     setTimeout(show);
-
     function show() {
+        $('#gt-header-registered').addClass('gt-open');
         enterForm.addClass('gt-open');
         enterClose.addClass('gt-open');
     }
@@ -11,8 +11,9 @@ this.openEnterForm = function () {
 this.closeEnterForm = function () {
     enterClose.removeClass('gt-open');
     enterForm.removeClass('gt-open');
+    $('#gt-header-registered').removeClass('gt-open');
+    self.closeRestorePass();
     setTimeout(hide, 750);
-
     function hide() {
         enterWrapper.removeClass('gt-open');
     }
@@ -25,6 +26,13 @@ this.changeEnterRegistration = function (event) {
 };
 
 
+this.openRestorePass = function () {
+    $('#gt-enter-pass-restore-wrapper').addClass('gt-open')
+
+};
+this.closeRestorePass = function () {
+    $('#gt-enter-pass-restore-wrapper').removeClass('gt-open')
+};
 
 
 enterButton.each(function () {
@@ -36,3 +44,6 @@ enterClose.on('click', self.closeEnterForm);
 enterChose.on('click', function (event) {
     self.changeEnterRegistration(event);
 });
+
+$('#gt-enter-form-restore-pass').on('click',self.openRestorePass);
+$('#gt-enter-pass-restore-submit').on('click',self.closeRestorePass);
